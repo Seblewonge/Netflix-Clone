1392c6d494a34bd2bd778cc598cc11a08acb2b38
seblewonge <seblewongelrcd@gmail.com>
2025-09-11 17:52:30 +0300

    deploy

--- netflex-clone/vite.config.js
+++ netflex-clone/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Netflix-Clone/",
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Netflix-Clone/", // 👈 must match repo name exactly
+});
 
 
 
