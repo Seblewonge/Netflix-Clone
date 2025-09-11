896b993bc27d12cf42dc661d2ff98a1e77ebc8a8
seblewonge <seblewongelrcd@gmail.com>
2025-09-11 17:59:59 +0300

    remove comment part code

--- netflex-clone/vite.config.js
+++ netflex-clone/vite.config.js
@@ -1,18 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/Netflix-Clone/",
-// });
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone/", // 👈 must match repo name exactly
+  base: "/Netflix-Clone/", 
 });
 
 
